f82a9f5407428c94c38e8a13c7899234f39dac6d
prd-tan-truong <tan.truong@paradox.ai>
2022-05-09 15:29:32 +0700

    Add - Townepark crawler

---
+++ crawler/crawler/spiders/townepark.py
@@ -0,0 +1,33 @@
+import scrapy
+import base64
+
+from crawler.items import Job
+
+
+class TowneparkSpider(scrapy.Spider):
+    # Metadata
+    name = 'townepark'
+
+    # Custom data
+    FEED_URL = ('https://wd5-impl-services1.workday.com/ccx/service/customreport2/townepark1/ISU_PDX_Jobs'
+                '/INT_PDX_Jobs?format=simplexml')
+    USERNAME = 'ISU_PDX_Jobs'
+    PASSWORD = 'April2022!!Paradox'
+
+    def start_requests(self):
+        basic_auth = f"Basic {base64.b64encode(f'{self.USERNAME}:{self.PASSWORD}'.encode('ASCII')).decode('utf-8')}"
+        self.logger.info(f'Basic Auth: {basic_auth}')
+        yield scrapy.Request(url=self.FEED_URL,
+                             headers={
+                                 'Authorization': basic_auth
+                             })
+
+    def parse(self, response, **kwargs):
+        response.selector.remove_namespaces()
+        jobs = response.xpath('//Report_Entry')
+        for job in jobs:
+            yield Job(
+                rid=job.xpath('.//jobid/text()').get(),
+                title=job.xpath('.//title/text()').get(),
+                url=job.xpath('.//apply-url/text()').get(),
+            )
