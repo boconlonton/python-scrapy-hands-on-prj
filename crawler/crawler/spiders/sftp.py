1f7d8ef2e6d7a59280d8fdc527cc7432adde8453
prd-tan-truong <tan.truong@paradox.ai>
2022-05-09 17:41:47 +0700

    Update - sftp_feed - Add parse() body

--- crawler/crawler/spiders/sftp.py
+++ crawler/crawler/spiders/sftp.py
@@ -2,6 +2,8 @@ from scrapy import Spider
 
 from scrapy.http import Request
 
+from crawler.items import Job
+
 
 class SftpSpider(Spider):
     # Metadata
@@ -23,4 +25,17 @@ class SftpSpider(Spider):
         yield Request(url=f'file://{self.FILENAME}')
 
     def parse(self, response, **kwargs):
-        pass
+        jobs = response.xpath('//job')
+
+        for job in jobs:
+            rid = job.xpath('.//jobID/text()').get()
+            title = job.xpath('.//title/text()').get()
+            category = job.xpath('.//category/text()').get()
+            description = job.xpath('.//description/text()').get()
+
+            yield Job({
+                'rid': rid,
+                'title': title,
+                'category_list': [category],
+                'description': description,
+            })
