de42667e9383d0224f33b7acccfa5956fd9c45b4
prd-tai-nguyen <91877469+prd-tai-nguyen@users.noreply.github.com>
2022-05-09 11:09:25 +0700

    [OL-54767] Hands-on: Dynamic website Feed (#5)
    * Crawler for xml
    
    * dynamic_website
    
    * [OL-54471] Hands-on Scrapy Framework

---
+++ crawler/crawler/spiders/dynamic_website.py
@@ -0,0 +1,36 @@
+from scrapy import Spider
+from scrapy.http import Request
+
+class DynamicWebsiteSpider(Spider):
+    name = 'dynamic_website'
+    allowed_domains = ['nuvasive.avature.net']
+    start_urls = ['https://nuvasive.avature.net/careers/SearchJobs/?jobOffset=0']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def parse(self, response, **kwargs):
+        jobs = response.xpath('//li[@class="jobResultItem"]')
+        for job in jobs:
+            url = job.xpath('.//h3/a/@href').extract_first()
+            yield Request(url=url, callback=self.parse_job)
+
+        # process next age
+        next_page_url = response.xpath('//a[@class="paginationItem paginationNextLink"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(url=absolute_next_page_url)
+
+    def parse_job(self, response, **kwargs):
+        url = response.url
+        title = response.xpath('//h2[@class="pageTitle1 color1 detailJob"]/text()').extract_first()
+        description = response.xpath('//div[@class="jobDetailDescription"]').extract_first()
+        apply_link = response.xpath('//div[@class="tPad1 jobButtonSet"]/a/@href').extract_first()
+
+        yield {
+            'rid': url.split("/")[-1],
+            'url': url,
+            'title': title,
+            'description': description,
+            'apply_link': apply_link,
+        }
+
