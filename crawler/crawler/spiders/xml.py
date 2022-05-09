c81448ab66c80839c2ad3978d452f0cbbfcf89ee
prd-tan-truong <tan.truong@paradox.ai>
2022-05-09 14:26:38 +0700

    Update - xml_spider - Use Item instead of raw json

--- crawler/crawler/spiders/xml.py
+++ crawler/crawler/spiders/xml.py
@@ -1,30 +1,27 @@
-
-from time import sleep
 from scrapy import Spider
-from scrapy import Request
-from scrapy.selector import Selector
+
+from crawler.items import Job
+
 
 class XmlSpider(Spider):
     name = 'xml'
     allowed_domains = ['www.appone.com']
-    start_urls = ['https://www.appone.com/branding/adfeed/default.asp?servervar=ValvolineInstantOilChange.AdFeed.appone.com&all=yes&accesscode=any']
-
+    start_urls = ['https://www.appone.com/branding/adfeed/default.asp?servervar=ValvolineInstantOilChange.AdFeed'
+                  '.appone.com&all=yes&accesscode=any']
 
     def parse(self, response, **kwargs):
         jobs = response.xpath('//Job')
-       
 
         for job in jobs:
-            id = job.xpath('.//JobID/text()').extract_first()
-            title = job.xpath('.//Title/text()').extract_first()
-            category = job.xpath('.//Category/text()').extract_first()
-            description = job.xpath('.//Description/text()').extract_first()
 
-            yield {
-                'id' : id,
+            rid = job.xpath('.//JobID/text()').get()
+            title = job.xpath('.//Title/text()').get()
+            category = job.xpath('.//Category/text()').get()
+            description = job.xpath('.//Description/text()').get()
+
+            yield Job({
+                'rid': rid,
                 'title': title,
-                'category': category,
+                'category_list': [category],
                 'description': description,
-            }
-
-        self.logger.info(len(jobs))
+            })
