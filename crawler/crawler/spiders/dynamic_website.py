1d88dbbed0d8b93c001f980aae966f8396258332
prd-tan-truong <tan.truong@paradox.ai>
2022-05-09 16:29:24 +0700

    Update - dynamic_website - change extract_first() to get()

--- crawler/crawler/spiders/dynamic_website.py
+++ crawler/crawler/spiders/dynamic_website.py
@@ -1,6 +1,7 @@
 from scrapy import Spider
 from scrapy.http import Request
 
+
 class DynamicWebsiteSpider(Spider):
     # Metadata
     name = 'dynamic_website'
@@ -8,26 +9,21 @@ class DynamicWebsiteSpider(Spider):
     start_urls = ['https://nuvasive.avature.net/careers/SearchJobs/?jobOffset=0']
 
     # Custom data
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def parse(self, response, **kwargs):
         jobs = response.xpath('//li[@class="jobResultItem"]')
         for job in jobs:
-            url = job.xpath('.//h3/a/@href').extract_first()
+            url = job.xpath('.//h3/a/@href').get()
             yield Request(url=url, callback=self.parse_job)
 
-        # process next age
-        next_page_url = response.xpath('//a[@class="paginationItem paginationNextLink"]/@href').extract_first()
+        next_page_url = response.xpath('//a[@class="paginationItem paginationNextLink"]/@href').get()
         absolute_next_page_url = response.urljoin(next_page_url)
         yield Request(url=absolute_next_page_url)
 
     def parse_job(self, response, **kwargs):
         url = response.url
-        title = response.xpath('//h2[@class="pageTitle1 color1 detailJob"]/text()').extract_first()
-        description = response.xpath('//div[@class="jobDetailDescription"]').extract_first()
-        apply_link = response.xpath('//div[@class="tPad1 jobButtonSet"]/a/@href').extract_first()
+        title = response.xpath('//h2[@class="pageTitle1 color1 detailJob"]/text()').get()
+        description = response.xpath('//div[@class="jobDetailDescription"]').get()
+        apply_link = response.xpath('//div[@class="tPad1 jobButtonSet"]/a/@href').get()
 
         yield {
             'rid': url.split("/")[-1],
