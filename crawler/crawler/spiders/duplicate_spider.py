e6c5a7162fe0acab70f06e77d7654d3156e09db1
prd-tan-truong <tan.truong@paradox.ai>
2022-05-17 11:24:26 +0700

    Update - duplicate_spider - Add sample for generically get_namespace from xml document

--- crawler/crawler/spiders/duplicate_spider.py
+++ crawler/crawler/spiders/duplicate_spider.py
@@ -3,33 +3,35 @@ from collections import defaultdict
 import scrapy
 
 # import json
-#
+
 # from lxml.etree import XMLParser
 # from lxml.etree import fromstring
-#
-# from parsel import Selector
 
 
 class DuplicateSpiderSpider(scrapy.Spider):
     # Metadata
     name = 'duplicate_spider'
-    start_urls = ['file:///home/tan/Downloads/Paradox_Job_Search_Bot_Sample_Feed.xml']
+    start_urls = ['https://mpwpublicdocs.blob.core.windows.net/silkroad/JobPosting.xml']
 
     # Custom data
 
     def parse(self, response, **kwargs):
+        # CDATA
         # parser = XMLParser(strip_cdata=False)
         # root = fromstring(response.body, parser=parser, base_url=response.url)
         # selector = Selector(root=root)
         # jobs = selector.xpath('//Report_Data//Report_Entry')
-        response.selector.register_namespace('wd', 'urn:com.workday.report/Paradox_Job_Search_Bot_Sample_Feed')
-        jobs = response.xpath('//wd:Report_Entry')
+        # Get namespace
+        # namespace_root = fromstring(response.text.encode('utf8'))
+        # for ns, val in namespace_root.nsmap.items():
+        #     response.selector.register_namespace(ns, val)
+        jobs = response.xpath('//z:row')
         dup_dict = defaultdict(int)
         id_set = set()
         cnt = 0
         for job in jobs:
             cnt += 1
-            job_id = job.xpath('./wd:Job_Req_ID/text()').get()
+            job_id = job.xpath('./@c2/text()').get()
             if job_id in id_set:
                 dup_dict[job_id] += 1
             else:
