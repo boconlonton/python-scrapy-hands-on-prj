1f5a20a4408f28155dd888e009b307f679ee05b7
prd-tan-truong <tan.truong@paradox.ai>
2022-05-17 18:03:31 +0700

    Update - townepark - follow RssSpider

--- crawler/crawler/spiders/rss/townepark.py
+++ crawler/crawler/spiders/rss/townepark.py
@@ -1,13 +1,14 @@
-from crawler.templates.private_rss import PrivateRssSpider
+from crawler.spiders.rss import RssSpider
 
 
-class TowneparkSpider(PrivateRssSpider):
+class TowneparkSpider(RssSpider):
     # Metadata
     name = 'townepark'
+    IS_PUBLIC = False
+    HAS_NAMESPACE = True
 
     # Custom data
-    FEED_URL = ('https://wd5-impl-services1.workday.com/ccx/service/'
-                'customreport2/townepark1/ISU_PDX_Jobs'
-                '/INT_PDX_Jobs?format=simplexml')
+    FEED_URL = ('https://wd5-impl-services1.workday.com/ccx/service/customr'
+                'eport2/townepark1/ISU_PDX_Jobs/INT_PDX_Jobs?format=simplexml')
     USERNAME = 'ISU_PDX_Jobs'
     PASSWORD = 'April2022!!Paradox'
